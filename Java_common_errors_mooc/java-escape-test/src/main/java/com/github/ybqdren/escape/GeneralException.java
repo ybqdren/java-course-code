55da9a4da692964d593a824793648ab987d0fedb
Wen Zhao (Joan) <withzhaowen@126.com>
2021-11-12 07:09:41 +0800

    update java-excape-test 使用java stream filter方法过滤集合

--- Java_common_errors_mooc/java-escape-test/src/main/java/com/github/ybqdren/escape/GeneralException.java
+++ Java_common_errors_mooc/java-escape-test/src/main/java/com/github/ybqdren/escape/GeneralException.java
@@ -1,8 +1,9 @@
 package com.github.ybqdren.escape;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Wen(Joan) Zhao <withzhaowen@126.com>
@@ -25,21 +26,30 @@ public class GeneralException {
     }
 
     // 直接使用for循环会触发并发修改异常
-    private static void concurrentModificationException(ArrayList<User> users){
+//    private static void concurrentModificationException(ArrayList<User> users){
+    private static List concurrentModificationException(ArrayList<User> users){
+
 /*        for(User user:users){
             if(user.getName().equals("ybqdren")){
                 users.remove(user);
             }
         }*/
 
-        // 使用迭代器则不会触发并发修改异常
+/*        // 使用迭代器则不会触发并发修改异常
         Iterator<User> iter = users.iterator();
         while (iter.hasNext()){
             User user = iter.next(); // 一定在remove之前调用 如果在其之后调用同样会报并发修改异常
             if(user.getName().equals("ybqdren")){
                 iter.remove();
             }
-        }
+        }*/
+
+        // 使用过滤器不会触发并发修改异常
+        Stream userStream = users.stream().filter(item -> item.equals(null));
+
+        List<User> userList = (ArrayList<User>) userStream.collect(Collectors.toList());
+
+        return userList;
     }
 
 
@@ -48,6 +58,8 @@ public class GeneralException {
         ArrayList<User> users = new ArrayList<>(
                 Arrays.asList(new User("wen"),new User("ybqdren"))
         );
-        concurrentModificationException(users);
+//        concurrentModificationException(users);
+
+        System.out.println(concurrentModificationException(users));
     }
 }
