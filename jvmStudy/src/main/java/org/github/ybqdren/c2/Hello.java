0f5023098ded8454416e236f3c229df33d5fc8e8
ybqdren <withzhaowen@126.com>
2021-12-08 14:03:46 +0800

    update

---
+++ jvmStudy/src/main/java/org/github/ybqdren/c2/Hello.java
@@ -0,0 +1,14 @@
+package org.github.ybqdren.c2;
+
+/**
+ * Wen(Joan) Zhao <withzhaowen@126.com>
+ * 2021/12/2
+ */
+public class Hello {
+    private static String msg = "Good morning";
+
+    public static void main( String[] args )
+    {
+        System.out.println( "msg==" + msg );
+    }
+}
