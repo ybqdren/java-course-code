7d4eb6c312cef0838549b71b4cf29ad9ae3df0e2
ybqdren <withzhaowen@126.com>
2021-12-22 19:09:36 +0800

    update 弱引用

---
+++ c3_1003/src/main/java/com/github/ybqdren/cg/TraceCanReliveObj.java
@@ -0,0 +1,12 @@
+package com.github.ybqdren.cg;
+
+/**
+ * @author Wen(Joan) Zhao
+ * @version v1.0
+ * @description 示例 4-6 使用虚引用跟踪一个可复活对象的回收
+ **/
+public class TraceCanReliveObj {
+    public static void main(String[] args) {
+        // code
+    }
+}
