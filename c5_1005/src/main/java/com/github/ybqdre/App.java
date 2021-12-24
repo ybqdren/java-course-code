70eb9c472395f9d88e1d0dcb31e5c04555ff60fd
ybqdren <withzhaowen@126.com>
2021-12-24 11:25:55 +0800

    update

---
+++ c5_1005/src/main/java/com/github/ybqdre/App.java
@@ -0,0 +1,13 @@
+package com.github.ybqdre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
