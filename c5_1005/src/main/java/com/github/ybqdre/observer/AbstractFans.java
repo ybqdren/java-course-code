b18457d53ff3d3facb26a61d39bfe44f3933cfe9
ybqdren <withzhaowen@126.com>
2021-12-29 12:42:14 +0800

    update 观察者模式

---
+++ c5_1005/src/main/java/com/github/ybqdre/observer/AbstractFans.java
@@ -0,0 +1,10 @@
+package com.github.ybqdre.observer;
+
+/**
+ * @author Wen(Joan) Zhao
+ * @time 2021/12/29 12:40
+ * @package com.github.ybqdre.observer
+ * @description
+ **/
+public class AbstractFans {
+}
