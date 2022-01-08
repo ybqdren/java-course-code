b7f7fb6beb437b60d6ff81ea30eb0be10f2a8d8f
Wen Zhao (Joan) <withzhaowen@126.com>
2022-01-08 10:26:29 +0800

    update

--- c8_1008/c8_1008_2/src/main/java/com/github/ybqdren/baseannotation/config/MyConfig.java
+++ c8_1008/c8_1008_2/src/main/java/com/github/ybqdren/baseannotation/config/MyConfig.java
@@ -2,6 +2,7 @@ package com.github.ybqdren.baseannotation.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Wen(Joan) Zhao
@@ -17,9 +18,17 @@ import org.springframework.context.annotation.Configuration;
     配置类内部包含一个或多个被 @Bean 注解的方法，这些方法将会被 AnnotationConfigApplicationContext
         或 AnnotationConfigWebApplicationContext 类进行扫描
     并用于构建 bean 的定义对象，初始化 Spring 容器
+
+注解 {@link Import}：
+    - 相当于 spring 配置文件中的 <import> 标签
+    - 用来组合多个配置类，在引入其他配置类时，可以不用再写 @Configuration 注解。当然，写上也没有问题
+
+    属性：
+        - value： 用来指定其他配置类的字节码文件
  */
 @Configuration
 @ComponentScan(basePackages = {"com.github.ybqdren.baseannotation.service"})
+@Import({MyProperties.class})
 public class MyConfig {
     public MyConfig() {
         System.out.println("IOC 容器初始化");
