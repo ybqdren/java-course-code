b6768dfb77b40b9f332a5b9634bc147c9fc80074
ybqdren <withzhaowen@126.com>
2022-01-08 15:35:54 +0800

    update 509

--- c12_1012/src/main/java/com/github/ybqdren/chapter1/dynamicprogramming/p509/other/solved1.java
+++ c12_1012/src/main/java/com/github/ybqdren/chapter1/dynamicprogramming/p509/other/solved1.java
@@ -7,9 +7,27 @@ package com.github.ybqdren.chapter1.dynamicprogramming.p509.other;
  * @description
  **/
 public class solved1 {
-
     public static void main(String[] args) {
 
+    }
+
+
+    // 方法三：通项公式
+
+
+    // 方法二：矩阵快速幂
+
+
+    // 方法一：动态规划
+    public static int fib_1(int n) {
+        if(n < 2){return n;}
+        int p = 0,q = 0,r = 1;
+        for(int i = 2 ; i<= n ; ++i){
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
     }
 
 }
