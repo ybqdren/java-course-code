b06fea8fb156b4b09108d9bba8e07d40635565d9
ybqdren <withzhaowen@126.com>
2022-01-17 16:29:09 +0800

    update

---
+++ c28_1028/passbook/src/main/java/com/github/ybqdren/passbook/PassbookApplication.java
@@ -0,0 +1,13 @@
+package com.github.ybqdren.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassbookApplication.class, args);
+    }
+
+}
---
+++ c28_1028/passbook/src/test/java/com/github/ybqdren/passbook/PassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ybqdren.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
