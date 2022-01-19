14b2347a2d5e4c68c11365ac3014cd55e2ca29ef
ybqdren <withzhaowen@126.com>
2022-01-19 10:34:15 +0800

    update

---
+++ c18_1018/src/test/java/com/github/ybqdren/TacoCloudApplication.java
@@ -0,0 +1,18 @@
+package com.github.ybqdren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wen(Joan) Zhao
+ * @time 2022/1/19 10:29
+ * @package com.github.ybqdren
+ * @description
+ **/
+
+@SpringBootApplication
+public class TacoCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class);
+    }
+}
