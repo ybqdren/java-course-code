6d65320c9277e542f2515651f16497c7d654d5bf
ybqdren <withzhaowen@126.com>
2022-01-19 09:44:55 +0800

    update

---
+++ cblog_test_2022/src/main/java/com/github/ybqdren/jpa/SpringDataJpaApplication.java
@@ -0,0 +1,20 @@
+package com.github.ybqdren.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wen(Joan) Zhao
+ * @time 2022/1/19 9:41
+ * @package com.github.ybqdren.jpa
+ * @description
+ * 参考：
+ *      - https://juejin.cn/post/7040817945202655245 步步深入Spring Data JPA系列（一）
+ **/
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApplication.class);
+    }
+}
