13abe0e94fe3d570f1b0d9e21b10cd9ee02b0524
ybqdren <withzhaowen@126.com>
2022-01-19 11:03:18 +0800

    update

---
+++ c29_2029/src/main/java/com/github/ybqdren/SpringDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.github.ybqdren;
+
+/**
+ * @author Wen(Joan) Zhao
+ * @time 2022/1/19 10:59
+ * @package com.github.ybqdren
+ * @description
+ **/
+
+
+public class SpringDataJpaApplication {
+}
