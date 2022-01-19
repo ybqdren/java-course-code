13a36d7b7b27960a7a49fba1d3ea695d12229584
ybqdren <withzhaowen@126.com>
2022-01-19 09:46:22 +0800

    update

--- cblog_test_2022/src/main/java/com/github/ybqdren/jpa/SpringDataJpaApplication.java
+++ cblog_test_2022/src/main/java/com/github/ybqdren/jpa/SpringDataJpaApplication.java
@@ -10,6 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  * 参考：
  *      - https://juejin.cn/post/7040817945202655245 步步深入Spring Data JPA系列（一）
+ *      - https://juejin.cn/post/7041222726828163103 步步深入Spring Data JPA系列（二）
+ *      - https://juejin.cn/post/7041911356580167693 步步深入Spring Data JPA系列（三）
+ *      - https://juejin.cn/post/7042120054065856549 步步深入Spring Data JPA系列（四）
+ *      - https://juejin.cn/post/7042308928541032456 步步深入Spring Data JPA系列（五）
  **/
 
 @SpringBootApplication
