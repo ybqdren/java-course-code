76b2a0f4b9ed41f813a1f5c4c2708765bc9c2e3b
ybqdren <withzhaowen@126.com>
2022-01-19 10:57:55 +0800

    update

---
+++ c29_2029/src/main/java/com/github/ybqdren/c29_2029/C292029Application.java
@@ -0,0 +1,13 @@
+package com.github.ybqdren.c29_2029;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C292029Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C292029Application.class, args);
+    }
+
+}
