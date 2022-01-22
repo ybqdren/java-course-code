3c7ec6ab768c9659e2409eba18e65c6d53e15f20
Wen Zhao (Joan) <withzhaowen@126.com>
2022-01-22 10:44:26 +0800

    update 商户实体对象定义

---
+++ c28_1028/passbook/src/main/java/com/github/ybqdren/passbook/dao/MerchantsDao.java
@@ -0,0 +1,37 @@
+package com.github.ybqdren.passbook.dao;
+
+import com.github.ybqdren.passbook.entity.Merchants;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author zhao wen
+ * @since 1.0.0
+ * <h1> Merchants Dao 接口 </h1>
+ *
+ * <p> 商户部分通用的可以放在一个新的模块中，方便重用 </p>
+ **/
+public interface MerchantsDao extends JpaRepository<Merchants, Integer> {
+
+    /**
+     * <h2> 通过 id 获取商户对象 </h2>
+     * @param id 商户 id
+     * @return {@link Merchants}
+     */
+    Merchants findbyId(Integer id);
+
+    /**
+     * <h2> 根据商户名称获取商户对象 </h2>
+     * @param name 商户名称
+     * @return {@link Merchants}
+     */
+    Merchants findByName(String name);
+
+    /**
+     * <h2> 根据商户 ids 获取商户对象 </h2>
+     * @param ids 商户 ids
+     * @return {@link Merchants}
+     */
+    List<Merchants> findByIdIn( List<Integer> ids);
+}
---
+++ c28_1028/passbook/src/main/java/com/github/ybqdren/passbook/entity/Merchants.java
@@ -0,0 +1,57 @@
+package com.github.ybqdren.passbook.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+/**
+ * @author zhao wen
+ * @since 1.0.0
+ * <h1> 商户对象模型 </h1>
+ **/
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "merchants")
+public class Merchants {
+    /** 商户 id */
+    @Id
+    @GeneratedValue
+    @Column(name = "id" , nullable = false)
+    private Integer id;
+
+    /** 商户名称 */
+    @Basic
+    @Column(name = "name" , unique = true , nullable = false)
+    private String name;
+
+    /** 商户 logo */
+    @Basic
+    @Column(name = "logo_url" , nullable = false)
+    private String logoUrl;
+
+    /** 商户营业执照 */
+    @Basic
+    @Column(name = "business_license_url" , nullable = false)
+    private String businessLicenseUrl;
+
+    /** 商户联系电话 */
+    @Basic
+    @Column(name = "phone" , nullable = false)
+    private String phone;
+
+    /** 商户地址 */
+    @Basic
+    @Column(name = "address" , nullable = false)
+    private String address;
+
+    /** 商户是否通过审核 */
+    @Basic
+    @Column(name = "is_audit" , nullable = false)
+    private String is_audit;
+}
