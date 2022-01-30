54162b4b62209eceb87341c579a481f966b95da6
Wen Zhao (Joan) <withzhaowen@126.com>
2022-01-30 21:05:03 +0800

    update

--- c33_1033/i-send-server/src/main/java/com/github/ybqdren/isendserver/ISendServerApplication.java
+++ c33_1033/i-send-server/src/main/java/com/github/ybqdren/isendserver/ISendServerApplication.java
@@ -2,7 +2,10 @@ package com.github.ybqdren.isendserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+//@ComponentScan(basePackages = {"com.github.ybqdren"}) // 扫描所有需要的包，包括一些自用的工具类包所在的路径
 @SpringBootApplication
 public class ISendServerApplication {
 
