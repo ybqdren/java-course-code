ef44f82fcb941a6e40b65f35ae38e5affb580410
Wen Zhao (Joan) <withzhaowen@126.com>
2022-01-30 16:55:04 +0800

    update

---
+++ c33_1033/i-send-server/src/main/java/com/github/ybqdren/isendserver/ISendServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.ybqdren.isendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ISendServerApplication.class, args);
+    }
+
+}
---
+++ c33_1033/i-send-server/src/test/java/com/github/ybqdren/isendserver/ISendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ybqdren.isendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISendServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
