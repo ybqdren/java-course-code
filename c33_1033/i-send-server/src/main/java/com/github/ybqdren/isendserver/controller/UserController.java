56535a0710ecb9fcfad6bd544b8d2fb84d25b2fb
Wen Zhao (Joan) <withzhaowen@126.com>
2022-01-31 15:36:54 +0800

    update

--- c33_1033/i-send-server/src/main/java/com/github/ybqdren/isendserver/controller/UserController.java
+++ c33_1033/i-send-server/src/main/java/com/github/ybqdren/isendserver/controller/UserController.java
@@ -1,9 +1,32 @@
 package com.github.ybqdren.isendserver.controller;
 
+import com.github.ybqdren.isendserver.pojo.Users;
+import com.github.ybqdren.isendserver.utils.IMoocJSONResult;
+import io.netty.util.internal.StringUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author zhao wen
  * @since 1.0.0
  **/
-public class UserController {
 
+@RestController
+@RequestMapping("u")
+public class UserController {
+    @PostMapping("/registOrLogin")
+    public IMoocJSONResult registOrLogin(@RequestBody Users user){
+
+        // 0.判断用户名和密码不能为空
+        if(StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())){
+            return IMoocJSONResult.errorMsg("用户名或密码不能为空 ...");
+        }
+
+
+
+        return IMoocJSONResult.ok();
+    }
 }
---
+++ c33_1033/i-send-server/src/main/java/com/github/ybqdren/isendserver/service/UserService.java
@@ -0,0 +1,10 @@
+package com.github.ybqdren.isendserver.service;
+
+/**
+ * @author zhao wen
+ * @since 1.0.0
+ **/
+public interface UserService {
+
+    public boolean queryUesrnameIsExist(String username);
+}
