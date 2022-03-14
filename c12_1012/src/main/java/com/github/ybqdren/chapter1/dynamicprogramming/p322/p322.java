56767ec07e6df45b3f865d5253619df528a64678
Wen Zhao (Joan) <withzhaowen@126.com>
2022-03-14 23:44:25 +0800

    update

---
+++ c12_1012/src/main/java/com/github/ybqdren/chapter1/dynamicprogramming/p322/p322.java
@@ -0,0 +1,24 @@
+package com.github.ybqdren.chapter1.dynamicprogramming.p322;
+
+/**
+ * <h1>  凑零钱 </h1>
+ *
+ * @Author zhao wen
+ * @Version 0.0.1
+ * @Date 2022/3/14
+ * https://leetcode-cn.com/problems/coin-change/
+ **/
+public class p322 {
+
+    /**
+     暴力递归
+     首先，这个问题是动态规划问题，因为它具有「最优子结构」的。
+     要符合「最优子结构」，子问题间必须互相独立。
+
+     
+
+     */
+    public void coin_1(){
+
+    }
+}
