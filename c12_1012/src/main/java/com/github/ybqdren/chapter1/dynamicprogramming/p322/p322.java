17bcae032558ca81dd95a237d5f7d9748b02d4c3
Wen Zhao (Joan) <withzhaowen@126.com>
2022-03-16 21:19:59 +0800

    update

--- c12_1012/src/main/java/com/github/ybqdren/chapter1/dynamicprogramming/p322/p322.java
+++ c12_1012/src/main/java/com/github/ybqdren/chapter1/dynamicprogramming/p322/p322.java
@@ -1,5 +1,7 @@
 package com.github.ybqdren.chapter1.dynamicprogramming.p322;
 
+import java.util.Arrays;
+
 /**
  * <h1>  凑零钱 </h1>
  *
@@ -10,15 +12,20 @@ package com.github.ybqdren.chapter1.dynamicprogramming.p322;
  **/
 public class p322 {
 
-    /**
-     暴力递归
-     首先，这个问题是动态规划问题，因为它具有「最优子结构」的。
-     要符合「最优子结构」，子问题间必须互相独立。
-
-     
-
-     */
-    public void coin_1(){
-
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    // 跳过无解的子问题
+                    if(i - coins[j] < 0 ){continue;}
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
